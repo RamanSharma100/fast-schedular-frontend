a0ae154d6854af2e4401ade3df63710b8a5c4678
RakeshPotnuru <rakeshpotnuru321@gmail.com>
2022-09-03 18:04:33 +0530

    Add register page
    Signed-off-by: RakeshPotnuru <rakeshpotnuru321@gmail.com>

---
+++ src/views/theme/index.js
@@ -0,0 +1,19 @@
+import { extendTheme, theme as base } from '@chakra-ui/react'
+
+const colors = {
+  brand: {
+    50: '#2C67F7',
+  },
+  twitter: '#1DA1F2',
+  linkedin: '#0077B5',
+  facebook: '#1877F2',
+}
+
+const fonts = {
+  heading: `Poppins, ${base.fonts?.heading}`,
+  body: `Poppins, ${base.fonts?.body}`,
+}
+
+const theme = extendTheme({ colors, fonts })
+
+export default theme
