3adbd4ce9c8fbbf5917e84a907a8baecc6bba514
RakeshPotnuru <rakeshpotnuru321@gmail.com>
2022-09-03 19:13:18 +0530

    7:13
    Signed-off-by: RakeshPotnuru <rakeshpotnuru321@gmail.com>

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,70 @@
 import React from 'react'
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  FormControl,
+  FormLabel,
+  Heading,
+  HStack,
+  Image,
+  Input,
+  Link,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
+import { FcGoogle } from 'react-icons/fc'
 
-const Login = () => {
-  return <div>Login</div>
+import Logo from '../../assets/brand/logo.png'
+
+const Register = () => {
+  return (
+    <Container size={'sm'} h={'100vh'}>
+      <Box m={8}>
+        <Center>
+          <VStack spacing={8}>
+            <HStack>
+              <Image src={Logo} alt={'fastschedular logo'} />
+              <Heading color={'brand.50'}>FastSchedular</Heading>
+            </HStack>
+            <Heading size={'md'}>Welcome back!</Heading>
+          </VStack>
+        </Center>
+        <Box boxShadow={'lg'} p={12} rounded={'md'}>
+          <VStack spacing={4}>
+            <FormControl>
+              <FormLabel>Email address</FormLabel>
+              <Input type={'email'} bg={'#E5ECFE'} />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Password</FormLabel>
+              <Input type={'password'} bg={'#E5ECFE'} />
+            </FormControl>
+            <Button bg={'brand.50'} color={'white'} _hover={{ opacity: 0.8 }}>
+              Login
+            </Button>
+            <Heading size={'md'}>OR</Heading>
+            <Button
+              leftIcon={<FcGoogle />}
+              w={'full'}
+              bg={'white'}
+              border={'2px solid gray'}
+              _hover={{ opacity: 0.8 }}
+            >
+              Sign in with Google
+            </Button>
+            <Text>
+              Don't have an account?{' '}
+              <Link href={'/register'} color={'brand.50'}>
+                Sign up
+              </Link>
+            </Text>
+          </VStack>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
-export default Login
+export default Register
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -7,6 +7,8 @@ import {
   FormControl,
   FormLabel,
   Heading,
+  HStack,
+  Image,
   Input,
   Link,
   Text,
@@ -14,41 +16,64 @@ import {
 } from '@chakra-ui/react'
 import { FcGoogle } from 'react-icons/fc'
 
+import Logo from '../../assets/brand/logo.png'
+
 const Register = () => {
   return (
     <Container size={'sm'}>
-      <Center m={8}>
-        <VStack spacing={8}>
-          <Heading color={'brand.50'}>FastSchedule</Heading>
-          <Heading size={'md'}>Sign Up for Fast Schedule</Heading>
-        </VStack>
-      </Center>
-      <Box boxShadow={'lg'} p={12} rounded={'md'}>
-        <VStack spacing={4}>
-          <FormControl>
-            <FormLabel>Email address</FormLabel>
-            <Input type={'email'} bg={'#E5ECFE'} />
-          </FormControl>
-          <Button bg={'brand.50'} color={'white'} _hover={{ opacity: 0.8 }}>
-            Get Started
-          </Button>
-          <Heading size={'md'}>OR</Heading>
-          <Button
-            leftIcon={<FcGoogle />}
-            w={'full'}
-            bg={'white'}
-            border={'2px solid gray'}
-            _hover={{ opacity: 0.8 }}
-          >
-            Sign up with Google
-          </Button>
-          <Text>
-            Already have an account?{' '}
-            <Link href={'/login'} color={'brand.50'}>
-              Login
-            </Link>
-          </Text>
-        </VStack>
+      <Box m={8}>
+        <Center>
+          <VStack spacing={8}>
+            <HStack>
+              <Image src={Logo} alt={'fastschedular logo'} />
+              <Heading color={'brand.50'}>FastSchedular</Heading>
+            </HStack>
+            <Heading size={'md'}>Sign Up for Fast Schedule</Heading>
+          </VStack>
+        </Center>
+        <Box boxShadow={'lg'} p={[8, 12]} rounded={'md'}>
+          <VStack spacing={4}>
+            <form>
+              <VStack spacing={4}>
+                <FormControl>
+                  <FormLabel>Email address</FormLabel>
+                  <Input type={'email'} bg={'#E5ECFE'} />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Full Name</FormLabel>
+                  <Input type={'text'} bg={'#E5ECFE'} />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Create a strong password</FormLabel>
+                  <Input type={'password'} bg={'#E5ECFE'} />
+                </FormControl>
+                <Button
+                  bg={'brand.50'}
+                  color={'white'}
+                  _hover={{ opacity: 0.8 }}
+                >
+                  Get Started
+                </Button>
+              </VStack>
+            </form>
+            <Heading size={'md'}>OR</Heading>
+            <Button
+              leftIcon={<FcGoogle />}
+              w={'full'}
+              bg={'white'}
+              border={'2px solid gray'}
+              _hover={{ opacity: 0.8 }}
+            >
+              Sign up with Google
+            </Button>
+            <Text>
+              Already have an account?{' '}
+              <Link href={'/login'} color={'brand.50'}>
+                Login
+              </Link>
+            </Text>
+          </VStack>
+        </Box>
       </Box>
     </Container>
   )
